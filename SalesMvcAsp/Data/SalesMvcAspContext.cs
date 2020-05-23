eeb79d2d5672ae64e98c5105cfa73bffb07ce0ad
igor helio <iggor1935@hotmail.com>
2020-05-23 17:01:30 -0300

    Scaffolfd Add item Controller

---
+++ SalesMvcAsp/Data/SalesMvcAspContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesMvcAsp.Models;
+
+namespace SalesMvcAsp.Data
+{
+    public class SalesMvcAspContext : DbContext
+    {
+        public SalesMvcAspContext (DbContextOptions<SalesMvcAspContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesMvcAsp.Models.Department> Department { get; set; }
+    }
+}
--- SalesMvcAsp/Startup.cs
+++ SalesMvcAsp/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using SalesMvcAsp.Data;
 
 namespace SalesMvcAsp
 {
@@ -33,6 +35,9 @@ namespace SalesMvcAsp
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SalesMvcAspContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SalesMvcAspContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
